6aa82c5b83f8ac564df79ac3b6f6d43967d5f42d
ookidoo <kid@mail.com>
2019-11-15 02:49:05 +0300

    test load

---
+++ JS/main.js
@@ -0,0 +1,58 @@
+const info = [];
+
+function createEl() {
+    const txt = document.createElement('span');
+    txt.classList.add('text');
+    document.body.appendChild(txt);
+    return txt;
+};
+
+function insertText(array) {
+    // const sepr = confirm('разделитель запятая?') ? ',' : "";
+    const span = createEl();
+    const text = array.join(`</br>`); // разделитель ${sepr}
+    span.innerHTML += text; 
+    console.dir(info);
+};
+
+function filterByBase(base, editNumbers) {
+    let final = [];
+    info[3] = 0;
+    editNumbers.forEach(el => {
+        for (let i = 0; i < base.length; i++) {
+            if (el === base[i]) {
+                info[3] += 1;
+                return;
+            };
+        }
+        final.push(el);
+    });
+    insertText(final);
+};
+
+const button = document.querySelector('.button');
+button.addEventListener('click', e => {
+    const userData = document.querySelector('.tel');
+    const editNumbers = [];
+    const data = userData.value.split('\n');
+    
+    data.forEach(el => {
+        if (el === '\n' || el === '' || el[0] === 'н') return;
+        editNumbers.push(`+38${el.substr(-10, 10)}`);
+    });
+    
+    info[0] = `вход: ${data.length}`;
+    info[1] = `ред.: ${editNumbers.length}`;
+    
+    fetch("../telbase.txt")
+    .then(response => response.text())
+    .then(text => text.split('\r\n'))
+    .then(base => {
+        button.style.display = 'none';
+        userData.style.display = 'none';
+
+        info[2] = `Base: ${base.length}`;
+        filterByBase(base, editNumbers);
+    });  
+}
+);
